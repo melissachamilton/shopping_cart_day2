40c4760ae701944872ded2c5ea55400c0f06ed92
melissachamilton <melissac.hamilton1@gmail.com>
2018-11-02 10:13:46 -0400

    completed set up

---
+++ src/Store.js
@@ -0,0 +1,7 @@
+export default(initialState) => {
+  return createStore(
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+// The Provider is a special type of component.
+// The Provider will wrap our App component along with a store that encapsulates
+// local states within the App component.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App'
+import Store from './Store'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const StoreInstance = Store()
+
+ReactDOM.render(
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
